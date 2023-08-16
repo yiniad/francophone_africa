421587c17778dc8b448b054f11bf92288352ca7f
yiniad <142420594+yiniad@users.noreply.github.com>
2023-08-16 15:21:46 +0100

    Add files via upload

---
+++ Imports and exports v2.R
@@ -0,0 +1,277 @@
+library(readxl)
+library(tidyverse)
+library(openxlsx)
+library(ggplot2)
+library(openxlsx)
+library(dplyr)
+
+co <- "Mali"
+exports <- read_excel(paste("/Users/adniaya/Downloads/",co,"_Exports_and_Imports_by_Areas_and_Co.xlsx",sep=""),sheet="Exports, FOB",skip=6)
+imports <- read_excel(paste("/Users/adniaya/Downloads/",co,"_Exports_and_Imports_by_Areas_and_Co.xlsx",sep=""),sheet="Imports, CIF",skip=6)
+
+
+exports <- exports[-1,]
+imports <- imports[-1,]
+#create sub categories for countries
+euro_area <- exports$Country[(which(exports$Country == "Euro Area")+1):(which(exports$Country =="Australia")-1)]
+emerging_and_developing_asia <- exports$Country[(which(exports$Country == "Emerging and Developing Asia")+1):(which(exports$Country =="Emerging and Developing Europe")-1)]
+emerging_and_developing_europe <- exports$Country[(which(exports$Country == "Emerging and Developing Europe")+1):(which(exports$Country =="Middle East and Central Asia")-1)]
+middle_east_and_central_asia <- exports$Country[(which(exports$Country == "Middle East and Central Asia")+1):(which(exports$Country =="Sub-Saharan Africa")-1)]
+subsaharan_africa <- exports$Country[(which(exports$Country == "Sub-Saharan Africa")+1):(which(exports$Country =="Western Hemisphere")-1)]
+western_emisphere <- exports$Country[(which(exports$Country == "Western Hemisphere")+1):(which(exports$Country =="Other Countries not included elsewhere")-1)]
+
+#create categories for countries
+advanced_economies <- c(euro_area, "Australia", "Canada","China, P.R.: Hong Kong","China, P.R.: Macao","Czech Rep.","Denmark","Iceland","Israel","Japan","Korea, Rep. of","New Zealand","Norway","San Marino, Rep. of","Singapore","Sweden","Switzerland","Taiwan Province of China","United Kingdom","United States","Holy See")
+emerging_and_developing_economies <- c(emerging_and_developing_asia,emerging_and_developing_europe,middle_east_and_central_asia, subsaharan_africa, western_emisphere)
+other_countries <- c("Cuba",	"Korea, Dem. People's Rep. of")
+countries_not_specified <- "Countries & Areas not specified"
+#add category and subcategory columns to imports and exports datasets 
+exports$category <- ifelse(exports$Country %in% advanced_economies, "Advanced Economy",
+                           ifelse(exports$Country %in% emerging_and_developing_economies, "Emerging and Developing Economies", 
+                                  ifelse(exports$Country %in% other_countries, "Other Countries not included elsewhere","N/A")))
+exports$subcategory <- ifelse(exports$Country %in% euro_area, "Euro Area",
+                              ifelse(exports$Country %in% emerging_and_developing_asia, "Emerging and Developing Asia",
+                                     ifelse(exports$Country %in% emerging_and_developing_europe, "Emerging and Developing Europe",
+                                            ifelse(exports$Country %in% middle_east_and_central_asia, "Middle East and Central Asia",
+                                                   ifelse(exports$Country %in% subsaharan_africa, "Sub-Saharan Africa",
+                                                          ifelse(exports$Country %in% western_emisphere, "Western Hemisphere",
+                                                                 "N/A"))))))
+
+imports$category <- ifelse(imports$Country %in% advanced_economies, "Advanced Economy",
+                           ifelse(imports$Country %in% emerging_and_developing_economies, "Emerging and Developing Economies", 
+                                  ifelse( imports$Country %in% countries_not_specified, "Countries & Areas not specified",
+                                          ifelse(imports$Country %in% other_countries, "Other Countries not included elsewhere","N/A"))))
+
+imports$subcategory <- ifelse(imports$Country %in% euro_area, "Euro Area",
+                              ifelse(imports$Country %in% emerging_and_developing_asia, "Emerging and Developing Asia",
+                                     ifelse(imports$Country %in% emerging_and_developing_europe, "Emerging and Developing Europe",
+                                            ifelse(imports$Country %in% middle_east_and_central_asia, "Middle East and Central Asia",
+                                                   ifelse(imports$Country %in% subsaharan_africa, "Sub-Saharan Africa",
+                                                          ifelse(imports$Country %in% western_emisphere, "Western Hemisphere",
+                                                                 "N/A"))))))
+
+
+#create dataset for breakdown by country only
+exports_by_country <- exports[exports$category != "N/A" , ]
+imports_by_country <- imports[imports$category != "N/A" , ]
+
+
+#reshape the dataset to long format and arrange by Import_Value
+exports_long <- exports_by_country %>%
+  gather(key = "Year", value = "Export_Value", `2000`:`2022`) %>%
+  arrange(Year)
+exports_long$Export_Value <- as.numeric(gsub("[^0-9.]", "",exports_long$Export_Value))
+
+imports_long <- imports_by_country %>%
+  gather(key = "Year", value = "Import_Value", `2000`:`2022`) %>%
+  arrange(Year)
+imports_long$Import_Value <- as.numeric(gsub("[^0-9.]", "",imports_long$Import_Value))
+
+exports_long$Year <- as.numeric(exports_long$Year)
+imports_long$Year <- as.numeric(imports_long$Year)
+
+imports_long <- imports_long %>%
+  mutate(subcategory = ifelse(subcategory == "N/A", "Other", subcategory))
+exports_long <- exports_long %>%
+  mutate(subcategory = ifelse(subcategory == "N/A", "Other", subcategory))
+
+# Total exports and imports by region
+total_exports_by_subcategory <- exports_long %>%
+  group_by(subcategory, Year) %>%
+  summarize(Total_Export = sum(Export_Value, na.rm = TRUE))
+total_imports_by_subcategory <- imports_long %>%
+  group_by(subcategory, Year) %>%
+  summarize(Total_Import = sum(Import_Value, na.rm = TRUE))
+total_by_sub_combined <- merge(total_exports_by_subcategory, total_imports_by_subcategory, by = c("subcategory", "Year"), all = TRUE)
+ggplot(total_by_sub_combined, aes(x = Year)) +
+  geom_line(aes(y = Total_Export, color = "Exports")) +
+  geom_line(aes(y = Total_Import, color = "Imports")) +
+  facet_wrap(~ subcategory, ncol = 2, scales = "free_y") +
+  labs(x = "Year", y = "Total Value",
+       title = paste(co, ": Evolution of Total Exports and Imports by Region in USD Mn")) +
+  scale_color_manual(values = c("Exports" = "blue", "Imports" = "red")) +
+  theme_minimal()
+
+
+# total exports for each year
+total_exports_by_year <- exports_long %>%
+  group_by(Year) %>%
+  summarize(Total_Export = sum(Export_Value, na.rm = TRUE))
+
+# total imports for each year
+total_imports_by_year <- imports_long %>%
+  group_by(Year) %>%
+  summarize(Total_Import = sum(Import_Value, na.rm = TRUE))
+
+# percentage of total exports and imports for each country per year
+exports_percentage <- exports_long %>%
+  group_by(Country, Year) %>%
+  summarise(Export_Percentage = (sum(Export_Value) / total_exports_by_year$Total_Export[match(Year, total_exports_by_year$Year)]) * 100)
+
+imports_percentage <- imports_long %>%
+  group_by(Country, Year) %>%
+  summarise(Import_Percentage = (sum(Import_Value, na.rm = TRUE) / total_imports_by_year$Total_Import[match(Year, total_imports_by_year$Year)]) * 100)
+
+# select the top 10 countries by percentage
+top_countries_exports <- exports_percentage %>%
+  group_by(Country) %>%
+  summarise(Total_Percentage = sum(Export_Percentage, na.rm = TRUE)) %>%
+  arrange(desc(Total_Percentage)) %>%
+  slice(1:10) %>%
+  pull(Country)
+top_countries_imports <- imports_percentage %>%
+  group_by(Country) %>%
+  summarise(Total_Percentage = sum(Import_Percentage, na.rm = TRUE)) %>%
+  arrange(desc(Total_Percentage)) %>%
+  slice(1:10) %>%
+  pull(Country)
+
+# filter data for top 10 countries
+top_exports <- exports_percentage %>%
+  filter(Country %in% top_countries_exports)%>%
+  arrange(Year,desc(Export_Percentage))
+
+top_imports <- imports_percentage %>%
+  filter(Country %in% top_countries_imports)%>%
+  arrange(Year,desc(Import_Percentage))
+
+# create the line plot
+ggplot(top_exports, aes(x = Year, y = Export_Percentage, color = Country)) +
+  geom_line() +
+  labs(x = "Year", y = "Percentage of Total Exports (%)",
+       title = "Evolution of Percentage of Total Exports by Country",
+       color = "Country") +
+  theme_minimal()
+ggplot(top_imports, aes(x = Year, y = Import_Percentage, color = Country)) +
+  geom_line() +
+  labs(x = "Year", y = "Percentage of Total Imports (%)",
+       title = "Evolution of Percentage of Total Imports by Country",
+       color = "Country") +
+  theme_minimal()
+
+# percentage of imports and exports from/to France
+imports_from_france <- imports_percentage %>%
+  filter(Country == "France")
+ggplot(imports_from_france, aes(x = Year, y = Import_Percentage)) +
+  geom_line( color = "red") +
+  labs(x = "Year", y = "Percentage out of Total Imports (%)",
+       title = paste(co,": Evolution of Percentage of Imports from France")) +
+  theme_minimal()
+
+exports_from_france <- exports_percentage %>%
+  filter(Country == "France")
+ggplot(exports_from_france, aes(x = Year, y = Export_Percentage)) +
+  geom_line(color = "blue") +
+  labs(x = "Year", y = "Percentage out of Total Exports (%)",
+       title = paste(co,": Evolution of Percentage of Exports to France")) +
+  theme_minimal()
+
+# percentage of imports and exports from/to France
+imports_from_morocco <- imports_percentage %>%
+  filter(Country == "Morocco")
+ggplot(imports_from_morocco, aes(x = Year, y = Import_Percentage)) +
+  geom_line( color = "red") +
+  labs(x = "Year", y = "Percentage out of Total Imports (%)",
+       title = paste(co,": Evolution of Percentage of Imports from Morocco")) +
+  theme_minimal()
+
+exports_from_morocco <- exports_percentage %>%
+  filter(Country == "Morocco")
+ggplot(exports_from_morocco, aes(x = Year, y = Export_Percentage)) +
+  geom_line(color = "blue") +
+  labs(x = "Year", y = "Percentage out of Total Exports (%)",
+       title = paste(co,": Evolution of Percentage of Exports to Morocco")) +
+  theme_minimal()
+
+# evolution of top countries percentages (unstacked)
+custom_colors <- c("#1f78b4", "#33a02c", "#e31a1c", "#ff7f00", "#6a3d9a",
+                   "#a6cee3", "#b2df8a", "#fb9a99", "#fdbf6f", "#cab2d6")
+
+ggplot(top_imports %>% filter(Year >= max(Year) - 6), aes(x = Year, y = Import_Percentage, fill = Country)) +
+  geom_bar(stat = "identity", position = "dodge") + 
+  labs(x = "Year", y = "Percentage of Total Import (%)",
+       title = paste(co, ": Top 10 Countries by Import Percentage"),
+       fill = "Country") +
+  scale_fill_manual(values = custom_colors) +
+  theme_minimal()
+
+ggplot(top_exports %>% filter(Year >= max(Year) - 6), aes(x = Year, y = Export_Percentage, fill = Country)) +
+  geom_bar(stat = "identity", position = "dodge") + 
+  labs(x = "Year", y = "Percentage of Total Export (%)",
+       title = paste(co, ": Top 10 Countries by Export Percentage"),
+       fill = "Country") +
+  scale_fill_manual(values = custom_colors) +
+  theme_minimal()
+
+# loop through lists of top countries (imports and exports) and create charts
+
+for (c in top_countries_imports) {
+  imports_from_c <- imports_percentage %>%
+    filter(Country == c)
+  p1 <- ggplot(imports_from_c, aes(x = Year, y = Import_Percentage)) +
+    geom_line(color = "red") +
+    labs(x = "Year", y = "Percentage out of Total Imports (%)",
+         title = paste(co, ": Evolution of Percentage of Imports from", c)) +
+    theme_minimal()
+  print(p1)  
+}
+
+for (c in top_countries_exports) {
+  exports_from_c <- exports_percentage %>%
+    filter(Country == c)
+  
+  p <- ggplot(exports_from_c, aes(x = Year, y = Export_Percentage)) +
+    geom_line(color = "blue") +
+    labs(x = "Year", y = "Percentage out of Total Exports (%)",
+         title = paste(co, ": Evolution of Percentage of Exports to", c)) +
+    theme_minimal()
+  
+  print(p)  
+}
+
+#loop through list of countries to be checked and create charts
+# check_countries <- c("Brazil", "India", "Bermuda")
+# for (c in check_countries){
+#   exports_from_c <- exports_percentage %>%
+#     filter(Country == c)
+#   p <- ggplot(exports_from_c, aes(x = Year, y = Export_Percentage)) +
+#     geom_line(color = "blue") +
+#     labs(x = "Year", y = "Percentage out of Total Exports (%)",
+#          title = paste(co,": Evolution of Percentage of Exports to", c)) +
+#     theme_minimal()
+#   print(p)
+# }
+# for (c in check_countries){
+#   imports_from_c <- imports_percentage %>%
+#     filter(Country == c)
+#   p <- ggplot(imports_from_c, aes(x = Year, y = Import_Percentage)) +
+#     geom_line(color = "red") +
+#     labs(x = "Year", y = "Percentage out of Total Imports (%)",
+#          title = paste(co,": Evolution of Percentage of Imports from", c)) +
+#     theme_minimal()
+#   print(p)
+# }
+# 
+# 
+# countries <- c("Mali","Niger","Senegal","Morocco","Algeria")
+# 
+# for (c in countries){
+#   exports_from_c <- exports_percentage %>%
+#     filter(Country == c)
+#   p <- ggplot(exports_from_c, aes(x = Year, y = Export_Percentage)) +
+#     geom_line(color = "blue") +
+#     labs(x = "Year", y = "Percentage out of Total Exports (%)",
+#          title = paste(co,": Evolution of Percentage of Exports to", c)) +
+#     theme_minimal()
+#   print(p)
+# }
+# for (c in countries){
+#   imports_from_c <- imports_percentage %>%
+#     filter(Country == c)
+#   p <- ggplot(imports_from_c, aes(x = Year, y = Import_Percentage)) +
+#     geom_line(color = "red") +
+#     labs(x = "Year", y = "Percentage out of Total Imports (%)",
+#          title = paste(co,": Evolution of Percentage of Imports from", c)) +
+#     theme_minimal()
+#   print(p)
+# }
---
+++ Imports and exports.R
@@ -0,0 +1,266 @@
+library(readxl)
+library(tidyverse)
+library(openxlsx)
+library(ggplot2)
+library(openxlsx)
+
+# Enter Country
+co <- "Mali"
+
+# Import data
+exports <- read_excel(paste("/Users/adniaya/Downloads/",co,"_Exports_and_Imports_by_Areas_and_Co.xlsx",sep=""),sheet="Exports, FOB",skip=6)
+imports <- read_excel(paste("/Users/adniaya/Downloads/",co,"_Exports_and_Imports_by_Areas_and_Co.xlsx",sep=""),sheet="Imports, CIF",skip=6)
+
+
+# Prepare data
+exports <- exports[-1,]
+imports <- imports[-1,]
+#create sub categories for countries
+euro_area <- exports$Country[(which(exports$Country == "Euro Area")+1):(which(exports$Country =="Australia")-1)]
+emerging_and_developing_asia <- exports$Country[(which(exports$Country == "Emerging and Developing Asia")+1):(which(exports$Country =="Emerging and Developing Europe")-1)]
+emerging_and_developing_europe <- exports$Country[(which(exports$Country == "Emerging and Developing Europe")+1):(which(exports$Country =="Middle East and Central Asia")-1)]
+middle_east_and_central_asia <- exports$Country[(which(exports$Country == "Middle East and Central Asia")+1):(which(exports$Country =="Sub-Saharan Africa")-1)]
+subsaharan_africa <- exports$Country[(which(exports$Country == "Sub-Saharan Africa")+1):(which(exports$Country =="Western Hemisphere")-1)]
+western_emisphere <- exports$Country[(which(exports$Country == "Western Hemisphere")+1):(which(exports$Country =="Other Countries not included elsewhere")-1)]
+
+#create categories for countries
+advanced_economies <- c(euro_area, "Australia", "Canada","China, P.R.: Hong Kong","China, P.R.: Macao","Czech Rep.","Denmark","Iceland","Israel","Japan","Korea, Rep. of","New Zealand","Norway","San Marino, Rep. of","Singapore","Sweden","Switzerland","Taiwan Province of China","United Kingdom","United States","Holy See")
+emerging_and_developing_economies <- c(emerging_and_developing_asia,emerging_and_developing_europe,middle_east_and_central_asia, subsaharan_africa, western_emisphere)
+other_countries <- c("Cuba",	"Korea, Dem. People's Rep. of")
+countries_not_specified <- "Countries & Areas not specified"
+#add category and subcategory columns to imports and exports datasets 
+exports$category <- ifelse(exports$Country %in% advanced_economies, "Advanced Economy",
+                           ifelse(exports$Country %in% emerging_and_developing_economies, "Emerging and Developing Economies", 
+                                  ifelse(exports$Country %in% other_countries, "Other Countries not included elsewhere","N/A")))
+exports$subcategory <- ifelse(exports$Country %in% euro_area, "Euro Area",
+                           ifelse(exports$Country %in% emerging_and_developing_asia, "Emerging and Developing Asia",
+                                  ifelse(exports$Country %in% emerging_and_developing_europe, "Emerging and Developing Europe",
+                                         ifelse(exports$Country %in% middle_east_and_central_asia, "Middle East and Central Asia",
+                                                ifelse(exports$Country %in% subsaharan_africa, "Sub-Saharan Africa",
+                                                       ifelse(exports$Country %in% western_emisphere, "Western Hemisphere",
+                                                              "N/A"))))))
+
+imports$category <- ifelse(imports$Country %in% advanced_economies, "Advanced Economy",
+                           ifelse(imports$Country %in% emerging_and_developing_economies, "Emerging and Developing Economies", 
+                                  ifelse( imports$Country %in% countries_not_specified, "Countries & Areas not specified",
+                                    ifelse(imports$Country %in% other_countries, "Other Countries not included elsewhere","N/A"))))
+
+imports$subcategory <- ifelse(imports$Country %in% euro_area, "Euro Area",
+                              ifelse(imports$Country %in% emerging_and_developing_asia, "Emerging and Developing Asia",
+                                     ifelse(imports$Country %in% emerging_and_developing_europe, "Emerging and Developing Europe",
+                                            ifelse(imports$Country %in% middle_east_and_central_asia, "Middle East and Central Asia",
+                                                   ifelse(imports$Country %in% subsaharan_africa, "Sub-Saharan Africa",
+                                                          ifelse(imports$Country %in% western_emisphere, "Western Hemisphere",
+                                                                 "N/A"))))))
+
+
+# create dataset for breakdown by country only
+exports_by_country <- exports[exports$category != "N/A" , ]
+imports_by_country <- imports[imports$category != "N/A" , ]
+
+
+# reshape the dataset to long format 
+exports_long <- exports_by_country %>%
+  gather(key = "Year", value = "Export_Value", `2000`:`2022`) %>%
+  arrange(Year)
+exports_long$Export_Value <- as.numeric(gsub("[^0-9.]", "",exports_long$Export_Value))
+
+imports_long <- imports_by_country %>%
+  gather(key = "Year", value = "Import_Value", `2000`:`2022`) %>%
+  arrange(Year)
+imports_long$Import_Value <- as.numeric(gsub("[^0-9.]", "",imports_long$Import_Value))
+
+exports_long$Year <- as.numeric(exports_long$Year)
+imports_long$Year <- as.numeric(imports_long$Year)
+
+imports_long <- imports_long %>%
+  mutate(subcategory = ifelse(subcategory == "N/A", "Other", subcategory))
+exports_long <- exports_long %>%
+  mutate(subcategory = ifelse(subcategory == "N/A", "Other", subcategory))
+
+#statistical analysis
+summary(exports_long$Export_Value)
+summary(imports_long$Import_Value)
+# Summary statistics by category
+exports_summary <- exports_long %>%
+  group_by(category) %>%
+  summarise(
+    total_exports = sum(Export_Value, na.rm = TRUE),
+    average_exports = mean(Export_Value, na.rm = TRUE),
+    max_exports = max(Export_Value, na.rm = TRUE),
+    min_exports = min(Export_Value, na.rm = TRUE)
+  )
+print(exports_summary)
+imports_summary <- imports_long %>%
+  group_by(category) %>%
+  summarise(
+    total_imports = sum(Import_Value, na.rm = TRUE),
+    average_exports = mean(Import_Value, na.rm = TRUE),
+    max_exports = max(Import_Value, na.rm = TRUE),
+    min_exports = min(Import_Value, na.rm = TRUE)
+  )
+print(imports_summary)
+
+#total imports and exports per year
+total_exports_by_year <- exports_long %>%
+  group_by(Year) %>%
+  summarize(Total_Export = sum(Export_Value, na.rm = TRUE))
+total_imports_by_year <- imports_long %>%
+  group_by(Year) %>%
+  summarize(Total_Import = sum(Import_Value, na.rm = TRUE))
+total_combined <- merge(total_exports_by_year, total_imports_by_year, by = "Year", all = TRUE)
+ggplot(total_combined, aes(x = Year)) +
+  geom_line(size=1.2, aes(y = Total_Export, color = "Exports")) +
+  geom_line(size=1.2, aes(y = Total_Import, color = "Imports")) +
+  labs(x = "Year", y = "Total Value",
+       title = paste(co,": Evolution of Total Exports and Imports by Year in USD mn")) +
+  scale_color_manual(values = c("Exports" = "blue", "Imports" = "red")) +
+  theme_minimal()
+
+
+
+#Exports and imports by subcategory
+total_exports_by_subcategory <- exports_long %>%
+  group_by(subcategory, Year) %>%
+  summarize(Total_Export = sum(Export_Value, na.rm = TRUE))
+
+total_imports_by_subcategory <- imports_long %>%
+  group_by(subcategory, Year) %>%
+  summarize(Total_Import = sum(Import_Value, na.rm = TRUE))
+total_by_sub_combined <- merge(total_exports_by_subcategory, total_imports_by_subcategory, by = c("subcategory", "Year"), all = TRUE)
+ggplot(total_by_sub_combined, aes(x = Year)) +
+  geom_line(aes(y = Total_Export, color = "Exports")) +
+  geom_line(aes(y = Total_Import, color = "Imports")) +
+  facet_wrap(~ subcategory, ncol = 2, scales = "free_y") +
+  labs(x = "Year", y = "Total Value",
+       title = paste(co, ": Evolution of Total Exports and Imports by Subcategoryin USD Mn")) +
+  scale_color_manual(values = c("Exports" = "blue", "Imports" = "red")) +
+  theme_minimal()
+
+ggplot(total_imports_by_subcategory, aes(x = Year, y = Total_Import, color = subcategory)) +
+  geom_line(size = 1.2) +
+  labs(x = "Year", y = "Total Import Value",
+       title = paste(co, ": Evolution of Total Imports by Subcategory in USD mn")) +
+  scale_color_discrete(name = "Subcategory") +
+  theme_minimal()
+
+ggplot(total_exports_by_subcategory, aes(x = Year, y = Total_Export, color = subcategory)) +
+  geom_line(size = 1.2) +
+  labs(x = "Year", y = "Total Export Value",
+       title = paste(co, ": Evolution of Total Exports by Subcategory in USD mn")) +
+  scale_color_discrete(name = "Subcategory") +
+  theme_minimal()
+
+# Exports and imports to France
+ggplot() +
+  geom_line(size = 1.2, data = exports_long %>% filter(Country == "France"),
+            aes(x = Year, y = Export_Value, color = "Exports")) +
+  geom_line(size = 1.2, data = imports_long %>% filter(Country == "France"),
+            aes(x = Year, y = Import_Value, color = "Imports")) +
+  labs(x = "Year", y = "Total Value",
+       title = paste(co, ": Evolution of Exports and Imports to/from France")) +
+  scale_color_manual(values = c("Exports" = "blue", "Imports" = "red")) +
+  theme_minimal()
+
+# Exports and imports to Morocco
+ggplot() +
+  geom_line(size = 1.2, data = exports_long %>% filter(Country == "Morocco"),
+            aes(x = Year, y = Export_Value, color = "Exports")) +
+  geom_line(size = 1.2, data = imports_long %>% filter(Country == "Morocco"),
+            aes(x = Year, y = Import_Value, color = "Imports")) +
+  labs(x = "Year", y = "Total Value",
+       title = paste(co, ": Evolution of Exports and Imports to/from Morocco")) +
+  scale_color_manual(values = c("Exports" = "blue", "Imports" = "red")) +
+  theme_minimal()
+
+#top countries stacked bars chart
+top_countries_exports <- exports_long %>%
+  #filter(Year >= max(Year) - 4) %>% 
+  group_by(Year, Country) %>%
+  summarize(Total_Export = sum(Export_Value, na.rm = TRUE)) %>%
+  arrange(Year, desc(Total_Export)) %>%
+  group_by(Year) %>%
+  mutate(Total_Value = sum(Total_Export)) %>%
+  mutate(Percentage = (Total_Export / Total_Value) * 100) %>%
+  slice(1:5) %>%
+  ungroup()
+
+ggplot(top_countries_exports, aes(x = Year, y = Percentage, fill = Country)) +
+  geom_bar(stat = "identity") +
+  labs(x = "Year", y = "Percentage of Total Export (%)",
+       title = paste(co,"Top 5 Countries by Export Percentage"),
+       fill = "Country") +
+  scale_fill_viridis_d() +  # Use a color scale
+  theme_minimal()
+
+top_countries_imports <- imports_long %>%
+  #filter(Year >= max(Year) - 4) %>% 
+  group_by(Year, Country) %>%
+  summarize(Total_Import = sum(Import_Value, na.rm = TRUE)) %>%
+  arrange(Year, desc(Total_Import)) %>%
+  group_by(Year) %>%
+  mutate(Total_Value = sum(Total_Import)) %>%
+  mutate(Percentage = (Total_Import / Total_Value) * 100) %>%
+  slice(1:5) %>%
+  ungroup()
+
+ggplot(top_countries_imports, aes(x = Year, y = Percentage, fill = Country)) +
+  geom_bar(stat = "identity") +
+  labs(x = "Year", y = "Percentage of Total Import (%)",
+       title = paste(co,"Top 5 Countries by Import Percentage"),
+       fill = "Country") +
+  scale_fill_viridis_d() +  # Use a color scale
+  theme_minimal()
+
+# years that feature france as top country
+years_with_france_imports <- top_countries_imports %>%
+  filter(Country == "France") %>%
+  select(Year, Percentage)
+
+years_with_france_exports <- top_countries_exports %>%
+  filter(Country == "France") %>%
+  select(Year, Percentage)
+
+#growth rate
+imports_france <- imports_long %>%
+  filter(imports_long$Country=="France")
+imports_france$Growth_Rate <- c(NA, diff(imports_france$Import_Value) / imports_france$Import_Value[-nrow(imports_france)] * 100)
+
+exports_france <- exports_long %>%
+  filter(exports_long$Country=="France")
+exports_france$Growth_Rate <- c(NA, diff(exports_france$Export_Value) / exports_france$Export_Value[-nrow(exports_france)] * 100)
+
+
+# total_exports_subsahara <- exports_long %>%
+#   filter(subcategory == "Sub-Saharan Africa")%>%
+#   group_by(Year) %>%
+#   summarize(Total_Export = sum(Export_Value, na.rm = TRUE))
+# 
+# total_imports_subsahara <- imports_long %>%
+#   filter(subcategory == "Sub-Saharan Africa") %>%
+#   group_by(subcategory, Year) %>%
+#   summarize(Total_Import = sum(Import_Value, na.rm = TRUE))
+# 
+# ggplot(total_exports_subsahara, aes(x = Year, y = Total_Export)) +
+#     geom_line(size = 1.2) +
+#     labs(x = "Year", y = "Total Export Value",
+#          title = paste(co, ": Evolution of Total Exports to Sub-Saharan Africa in USD mn")) +
+#     scale_color_discrete(name = "Subcategory") +
+#     theme_minimal()
+# 
+# ggplot(total_imports_subsahara, aes(x = Year, y = Total_Import)) +
+#     geom_line(size = 1.2) +
+#     labs(x = "Year", y = "Total Import Value",
+#          title = paste(co, ": Evolution of Total Imports from Sub-Saharan Africa in USD mn")) +
+#     scale_color_discrete(name = "Subcategory") +
+#     theme_minimal()
+
+
+# Export dataset (reshaped to long format)
+setwd(dir = "/Users/adniaya/Desktop/Trade data")
+wb <- createWorkbook()
+addWorksheet(wb, "Exports")
+writeData(wb, sheet = 1, x = exports_long)
+addWorksheet(wb, "Imports")
+writeData(wb, sheet = 2, x = imports_long)
+saveWorkbook(wb, paste(co,"Exports and Imports.xlsx"), overwrite = TRUE)
